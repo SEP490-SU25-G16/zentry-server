684a68e8bb1a97d4e9015f20e326c5a23f2c63ed
DaxDVN <dax.datnguyen@gmail.com>
2025-06-18 12:55:35 +0700

    chore(device-management): Add first init migration

---
+++ src/Zentry.Modules/DeviceManagement/Zentry.Modules.DeviceManagement.Infrastructure/Migrations/20250618041658_Initial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zentry.Modules.DeviceManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    DeviceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DeviceName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    DeviceToken = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    LastVerifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: false, defaultValueSql: "'Active'")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.DeviceId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_AccountId",
+                table: "Devices",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_DeviceToken",
+                table: "Devices",
+                column: "DeviceToken",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_Status",
+                table: "Devices",
+                column: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
