09b4e00ddc6b916cc8b28dcaaa0b01287ecd25bc
DaxDVN <dax.datnguyen@gmail.com>
2025-06-30 15:04:57 +0700

    chore(schedule): remove mock feature

--- src/Zentry.Modules/ScheduleManagement/Zentry.Modules.ScheduleManagement.Application/Features/ViewStudentSchedule/ViewStudentScheduleRequest.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-using Zentry.Modules.ScheduleManagement.Application.Dtos;
-
-namespace Zentry.Modules.ScheduleManagement.Application.Features.ViewStudentSchedule;
-
-public record ViewStudentScheduleRequest(DateTime StartDate, DateTime EndDate) : IRequest<List<ScheduleDto>>;
