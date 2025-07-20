dbe79884cf1f5c1710eeb781fdc87b9a7910fa9e
DaxDVN <dax.datnguyen@gmail.com>
2025-07-21 05:54:12 +0700

    feat(schedule + attendance): update entity field

---
+++ src/Zentry.Modules/AttendanceManagement/Zentry.Modules.AttendanceManagement.Infrastructure/Migrations/20250720222457_UpdateRoundEntity.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zentry.Modules.AttendanceManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRoundEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Rounds_DeviceId",
+                table: "Rounds");
+
+            migrationBuilder.DropColumn(
+                name: "ClientRequest",
+                table: "Rounds");
+
+            migrationBuilder.DropColumn(
+                name: "DeviceId",
+                table: "Rounds");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Rounds",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoundNumber",
+                table: "Rounds",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Rounds",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Rounds",
+                type: "timestamp with time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rounds_SessionId_RoundNumber",
+                table: "Rounds",
+                columns: new[] { "SessionId", "RoundNumber" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Rounds_SessionId_RoundNumber",
+                table: "Rounds");
+
+            migrationBuilder.DropColumn(
+                name: "RoundNumber",
+                table: "Rounds");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Rounds");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Rounds");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Rounds",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientRequest",
+                table: "Rounds",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeviceId",
+                table: "Rounds",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rounds_DeviceId",
+                table: "Rounds",
+                column: "DeviceId");
+        }
+    }
+}
