15df50f96e0dd27aab8119f96f6f8a1bfb0cb4c4
DaxDVN <dax.datnguyen@gmail.com>
2025-07-24 20:52:33 +0700

    chore(user): add migration

---
+++ src/Zentry.Modules/UserManagement/Zentry.Modules.UserManagement/Migrations/20250724135215_AddVirtualField.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zentry.Modules.UserManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVirtualField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Accounts_AccountId",
+                table: "Users",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Accounts_AccountId",
+                table: "Users");
+        }
+    }
+}
