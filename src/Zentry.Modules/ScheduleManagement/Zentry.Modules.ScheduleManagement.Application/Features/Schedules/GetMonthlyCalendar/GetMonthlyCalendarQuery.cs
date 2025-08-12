1500fb0018fbc608f06c7d6a5a824eb43b9f52b2
DaxDVN <dax.datnguyen@gmail.com>
2025-08-12 11:41:37 +0700

    feat(schedule): add feature student monthly calendar

--- src/Zentry.Modules/ScheduleManagement/Zentry.Modules.ScheduleManagement.Application/Features/Schedules/GetMonthlyCalendar/GetMonthlyCalendarQuery.cs
+++
@@ -1,7 +0,0 @@
-using Zentry.Modules.ScheduleManagement.Application.Dtos;
-using Zentry.SharedKernel.Abstractions.Application;
-
-namespace Zentry.Modules.ScheduleManagement.Application.Features.Schedules.GetMonthlyCalendar;
-
-public record GetMonthlyCalendarQuery(Guid LecturerId, int Month, int Year)
-    : IQuery<MonthlyCalendarResponseDto>;
