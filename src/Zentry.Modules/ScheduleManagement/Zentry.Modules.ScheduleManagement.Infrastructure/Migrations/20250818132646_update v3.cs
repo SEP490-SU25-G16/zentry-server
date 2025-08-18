130435526caea3365ac1fc368279967e8feba59c
DaxDVN <dax.datnguyen@gmail.com>
2025-08-18 20:26:57 +0700

    fix: add missing file

---
+++ src/Zentry.Modules/ScheduleManagement/Zentry.Modules.ScheduleManagement.Infrastructure/Migrations/20250818132646_update v3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zentry.Modules.ScheduleManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatev3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Rooms");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
